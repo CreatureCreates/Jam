2588c3354f83b7eb67cf8de18037fdac02030837
Joeri Smit <70571197+157964@users.noreply.github.com>
2022-07-12 00:51:49 +0200

    Fps Counter now has an off toggle

--- Unity/GameJam/Assets/Scripts/FpsCounterScript.cs
+++ Unity/GameJam/Assets/Scripts/FpsCounterScript.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class FpsCounterScript : MonoBehaviour
 {
-    public int avgFrameRate;
-    public string text;
+    //zet dit op 0 om de fps counter uit te zetten
+    public int TurnFpsCounterOn = 1;
+    //dit displayt het gemiddelde fps over de laatste 30 instances
+    public string AvgFrameRate;
+    //Een queue om de laatst berekende fps op te slaan
+    private Queue<int> lastFewAverages = new Queue<int>();
  
     public void Update ()
     {
+        if(TurnFpsCounterOn == 0)
+            return;
+
         float current = 0;
         current = (int)(1f / Time.unscaledDeltaTime);
-        avgFrameRate = (int)current;
-        text = avgFrameRate.ToString() + " FPS";
+        lastFewAverages.Enqueue((int)current);
 
+        if(lastFewAverages.Count >= 30) {
+            AvgFrameRate = (int)lastFewAverages.Average() + " FPS";
+            lastFewAverages.Dequeue();
+        }
     }
 }
